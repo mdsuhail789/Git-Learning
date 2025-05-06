8b6216332b0cb4ae0ef1174b75eb1a78b8a1e340
Suhail <mdsuhail63831@gmail.com>
2025-05-06 14:42:52 +0530

    Payment updated after footer

--- first.js
+++ first.js
@@ -4,5 +4,7 @@ console.log(button);
 const login = "Login page added";
 console.log(login);
 
+const Payment = "Integrated the payment gateway";
+console.log(Payment);
 const footer = "footer added in our website";
 console.log(footer);
